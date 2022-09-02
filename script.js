7d0fa9c0a1f6ebadb5e3058f44d4c192d7963ae9
Thales Barreto <trvbarreto@gmail.com>
2022-09-02 09:38:38 -0300

    Fix number of squares

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const motherOfDivs = document.querySelector('#container');
 function divGenerator() {
     let counter = 0;
 
-    while (counter < 16) {
+    while (counter < 256) {
         const div = document.createElement('div');
         div.style.width = '3rem';
         div.style.height = '3rem'
