b47402eee4925e4bb22b89c274099a7eeff15645
Thales Barreto <trvbarreto@gmail.com>
2022-09-02 09:52:37 -0300

    Add event listener on the child divs for changing bgcolor on hover

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ function divGenerator() {
     while (counter < 256) {
         const div = document.createElement('div');
         div.style.width = '3rem';
-        div.style.height = '3rem'
+        div.style.height = '3rem';
+
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = '#c08009';
+        });
 
         motherOfDivs.appendChild(div);        
 
